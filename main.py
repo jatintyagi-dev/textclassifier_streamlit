cafdc3ea4809523506114a3417b77ead6718f0a8
Jatin Tyagi <jatin.tyagi@toyotaconnected.eu>
2021-12-27 22:42:20 +0530

    Complete training script

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 import fastai
-from train import train_lm
+from train import train_lm, train_cls
 
 
 st.title("Text Classifier!")
@@ -10,15 +10,24 @@ file = st.file_uploader("Please upload the data for training ")
 
 
 if file:
-    inp = pd.DataFrame(file)
+    inp = pd.read_csv(file)
     st.dataframe(inp)
 
+    inp = inp[["Text", "Sentiment"]]
 
-train = st.sidebar.selectbox("Pick a process", ["Training ", "Inference"])
 
-if train == "Training":
+train = st.sidebar.selectbox(
+    "Pick a process", ["Training LM", "Training Classifier"])
+
+if train == "Training LM":
 
     LM = st.sidebar.selectbox("Train a Language Model", ["Yes ", "No"])
 
     if LM == "Yes":
-        lmlearner = train_lm()
+        lmlearner = train_lm(inp)
+        model_trained = train_cls(inp, train_lm=True)
+        st.header("Model Training Finished")
+
+else:
+    model_trained = train_cls(inp, train_lm=False)
+    st.header("Model Training Finished")
--- train.py
+++ train.py
@@ -1,2 +1,42 @@
-def train_lm():
+from fastai.text.all import *
+
+
+def train_lm(data):
+    dls = TextDataLoaders.from_df(data, is_lm=True)
+    learn = language_model_learner(
+        dls, AWD_LSTM, metrics=[accuracy, Perplexity()], wd=0.1).to_fp16()
+
+    learn.unfreeze()
+
+    learn.fit_one_cycle(10, 1e-3)
+
+    learn.save_encoder('finetuned')
+
+    return True
+
+
+def train_cls(data, train_lm):
+
+    dls = TextDataLoaders.from_df(data)
+    learn = text_classifier_learner(
+        dls, AWD_LSTM, drop_mult=0.5, metrics=accuracy)
+
+    if train_lm:
+        learn = learn.load_encoder("finetuned")
+
+    learn.fit_one_cycle(1, 2e-2)
+
+    learn.freeze_to(-2)
+
+    learn.fit_one_cycle(1, slice(1e-2/(2.6**4), 1e-2))
+
+    learn.freeze_to(-3)
+
+    learn.fit_one_cycle(1, slice(5e-3/(2.6**4), 5e-3))
+
+    learn.unfreeze()
+
+    learn.fit_one_cycle(2, slice(1e-3/(2.6**4), 1e-3))
+
+    save_model("txt_classifier", learn)
     return True
